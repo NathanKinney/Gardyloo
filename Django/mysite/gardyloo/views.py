1787e609008c287d6502a00b0e8b09b62320fe90
Nathan Kinney <nathankeithkinney@gmail.com>
2018-07-07 17:57:59 -0700

    v3 working on accessing db on home.view

--- Django/mysite/gardyloo/views.py
+++ Django/mysite/gardyloo/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from .models import Country, Language, CountryLanguage, Religion, CountryReligion, Article
-def index(request):
-    return HttpResponse("Hello, world. You're at the gardyloo index.")
+from .models import Country
+
+
+def home(request):
+
+    country = Country.objects.all()
+    context = {
+        'title': 'list of countries',
+        'country': country
+    }
+    return render(request, 'gardyloo/home.html', context)
