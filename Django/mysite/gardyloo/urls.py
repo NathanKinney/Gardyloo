87f2628df770334e097a1fa572896a4ce143b11b
Nathan Kinney <nathankeithkinney@gmail.com>
2018-07-09 11:26:19 -0700

    pictures uploading to proper places

--- Django/mysite/gardyloo/urls.py
+++ Django/mysite/gardyloo/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+
 from . import views
 
 app_name = 'gardyloo'
@@ -8,4 +7,4 @@ app_name = 'gardyloo'
 urlpatterns = [
     path('', views.home, name='home'),
     path('details/<int:country_id>', views.details, name='details'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
