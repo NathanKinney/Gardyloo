b05b7c3dc63ae6c9ad2a1cdff7d2ab36bc04d579
Nathan Kinney <nathankeithkinney@gmail.com>
2018-07-21 18:38:50 -0700

    More d3

--- D3/Mock Business/Coffee.js
+++ D3/Mock Business/Coffee.js
@@ -1,14 +1,14 @@
-var margin = { left:80, right:20, top:50, bottom:100 };
+var margin = { left:80, right:20, top:50, bottom:100 }
 
-var width = 600 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var width = 600 - margin.left - margin.right
+    height = 400 - margin.top - margin.bottom
 
 var g = d3.select("#chart-area")
     .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
     .append("g")
-        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
 
 // X Label
 g.append("text")
@@ -16,7 +16,7 @@ g.append("text")
     .attr("x", width / 2)
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
-    .text("Month");
+    .text("Month")
 
 // Y Label
 g.append("text")
@@ -25,40 +25,40 @@ g.append("text")
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
     .attr("transform", "rotate(-90)")
-    .text("Revenue");
+    .text("Revenue")
 
 d3.json("revenues.json").then(function(data){
-    console.log(data);
+    console.log(data)
 
     // Clean data
     data.forEach(function(d) {
-        d.revenue = +d.revenue;
-    });
+        d.revenue = +d.revenue
+    })
 
     // X Scale
     var x = d3.scaleBand()
         .domain(data.map(function(d){ return d.month }))
         .range([0, width])
-        .padding(0.2);
+        .padding(0.2)
 
     // Y Scale
     var y = d3.scaleLinear()
         .domain([0, d3.max(data, function(d) { return d.revenue })])
-        .range([height, 0]);
+        .range([height, 0])
 
     // X Axis
-    var xAxisCall = d3.axisBottom(x);
+    var xAxisCall = d3.axisBottom(x)
     g.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height +")")
-        .call(xAxisCall);
+        .call(xAxisCall)
 
     // Y Axis
     var yAxisCall = d3.axisLeft(y)
-        .tickFormat(function(d){ return "$" + d; });
+        .tickFormat(function(d){ return "$" + d })
     g.append("g")
         .attr("class", "y axis")
-        .call(yAxisCall);
+        .call(yAxisCall)
 
     // Bars
     var rects = g.selectAll("rect")
@@ -66,9 +66,9 @@ d3.json("revenues.json").then(function(data){
 
     rects.enter()
         .append("rect")
-            .attr("y", function(d){ return y(d.revenue); })
+            .attr("y", function(d){ return y(d.revenue) })
             .attr("x", function(d){ return x(d.month) })
-            .attr("height", function(d){ return height - y(d.revenue); })
+            .attr("height", function(d){ return height - y(d.revenue) })
             .attr("width", x.bandwidth)
-            .attr("fill", "grey");
+            .attr("fill", "grey")
 })
