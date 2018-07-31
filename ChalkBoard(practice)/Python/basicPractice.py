2549df7d2b13b9ddf7533a99ea44ddf06d2e3024
Nathan Kinney <nathankeithkinney@gmail.com>
2018-07-31 15:11:59 -0700

    python practice

---
+++ ChalkBoard(practice)/Python/basicPractice.py
@@ -0,0 +1,26 @@
+
+
+# employees = {'marge': 3, 'mag': 2}
+# employees['phil'] = '5'
+# print(employees.values())
+# new_list = list(iter(employees))
+# for key, value in employees.iteritems():
+#     print(key,value)
+# print(new_list)
+
+# t = ('a', 'b', 'c', 'd')
+# print(t.index('c'))
+
+# print(1 > 2)
+
+# myfile = open('test_file.txt')
+# read = myfile.read()
+# print(read)
+
+# with open('test_file.txt') as myfile:
+#     read = myfile.read()
+#     print(read)
+
+f = open('second_file.txt')
+lines = f.read()
+print(lines)
