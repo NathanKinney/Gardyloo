48c8a33d28ca2d8cce27a7a4549e34fc3c77161e
Nathan Kinney <nathankeithkinney@gmail.com>
2018-07-31 23:46:55 -0700

    python practice

---
+++ ChalkBoard(practice)/Python/Functions/pracitce2.py
@@ -0,0 +1,29 @@
+def square(num):
+    return num**2
+# MAP FUNCTIONS
+# my_nums = [1,2,3,4,5]
+
+# for item in map(square, my_nums):
+#     print(item)
+# print(list(map(square, my_nums)))
+
+# def splicer(mystring):
+#     if len(mystring)%2 == 0:
+#         return 'even'
+#     else:
+#         return mystring[0]
+#
+# mynames = ['A', 'BB', 'CCC','DDDD']
+#
+# print(list(map(splicer, mynames)))
+
+#FILTER FUNCTION
+nums = [1,2,3,4,5,6]
+# def check_even(num):
+#     return num%2 == 0
+# print(list(filter(check_even, nums)))
+
+# LAMBDA EXPRESSION
+# print(list(map(lambda num: num**2,nums )))
+
+
