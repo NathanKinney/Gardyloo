c40c4f24addccf36ea3e7a53512ed9d16baebf12
Nathan Kinney <nathankeithkinney@gmail.com>
2018-08-01 18:02:53 -0700

    python practice

--- ChalkBoard(practice)/Python/Functions/OOP.py
+++ ChalkBoard(practice)/Python/Functions/OOP.py
@@ -1,11 +1,49 @@
+import random
+#
+# class NameOfClass():
+#
+#     def __init__(self,param1, param2):
+#         self.param1 = param1
+#         self.param2 = param2
+#
+#     def some_method(self):
+#         #codeblock
+#          print(self.param1)
 
-class NameOfClass():
 
-    def __init__(self,param1, param2):
-        self.param1 = param1
-        self.param2 = param2
+# class Sphere():
+#
+#     def __init__(self, radius):
+#         self.radius = radius
+#
+#     def volume(self):
+#         return (4/3)*3.14*(self.radius**3)
+#
+#     def surface_area(self):
+#         return 4*3.14 * self.radius**2
+#
+# s = Sphere(3)
 
-    def some_method(self):
-        #codeblock
-         print(self.param1)
-
+# print(s.surface_area())
+#
+# class GuessingGame():
+#
+#     def __init__(self):
+#
+#         self.rand_choice = random.randint(0,10)
+#
+#     def reset_random(self):
+#         print('Reseting random number')
+#         self.rand_choice = random.randint(0,10)
+#
+#     def guess(self):
+#         user_guess = int(input('Please input a random number:'))
+#
+#         if user_guess == self.rand_choice:
+#             print('CORRECT!')
+#         else:
+#             print('Wrong Guess')
+#
+# g = GuessingGame
+#
+# print(g.guess(5))
