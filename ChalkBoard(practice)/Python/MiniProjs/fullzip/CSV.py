3252870b5ea733a141761ff80e98f22da83d568e
Nathan Kinney <nathankeithkinney@gmail.com>
2018-08-03 16:17:53 -0700

    csv append and create

--- ChalkBoard(practice)/Python/MiniProjs/fullzip/CSV.py
+++ ChalkBoard(practice)/Python/MiniProjs/fullzip/CSV.py
@@ -18,7 +18,12 @@ print(all_emails)
 
 file_to_output = open('madeup.csv', 'w', newline='')
 csv_writer = csv.writer(file_to_output, delimiter=',')
-csv_Writer.writerow(['col1', 'col2', 'col3'])
-csv_Writer.writerow([1,2,3],[4,5,6])
+csv_writer.writerow(['col1', 'col2', 'col3'])
+csv_writer.writerow([1,2,3])
 file_to_output.close()
+
+f = open('madeup.csv', 'a', newline='')
+csv_writer = csv.writer(f)
+csv_writer.writerow([7,8,9])
+f.close()
 
