46a869c5f54d831a93539e0c8e34ed288fd7c4fe
Nathan Kinney <nathankeithkinney@gmail.com>
2018-08-05 16:24:59 -0700

    Functioning APP Basic css

---
+++ WeatherApp/Elements.js
@@ -0,0 +1,12 @@
+// USER RELATIONS
+export const ELEMENT_SEARCH_BUTTON = document.querySelector('button');
+export const ELEMENT_SEARCHED_CITY = document.querySelector('#city');
+
+// LOADING ELEMENTs
+export const ELEMENT_LOADING_TEXT = document.querySelector('#load');
+export const ELEMENT_WEATHER_BOX = document.querySelector('#weather');
+
+// CITY ELEMENTS
+export const ELEMENT_WEATHER_CITY = ELEMENT_WEATHER_BOX.firstElementChild;
+export const ELEMENT_WEATHER_DESCRIPTION = document.querySelector('#weatherDescription');
+export const ELEMENT_WEATHER_TEMPERATURE = ELEMENT_WEATHER_BOX.lastElementChild;
