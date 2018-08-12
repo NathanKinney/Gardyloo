bb5b3955cce76e7af9c96402f5aaf69d8bae3f15
Nathan Kinney <nathankeithkinney@gmail.com>
2018-08-11 23:10:03 -0700

    Node Work

--- ChalkBoard(practice)/Node/NotesNode/app.js
+++ ChalkBoard(practice)/Node/NotesNode/app.js
@@ -1,28 +1,41 @@
-console.log('starting app.')
+console.log('Starting app.js');
 
-const fs = require('fs')
-const _ = require('lodash')
-const yargs = require('yargs')
-const notes = require('./notes.js')
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
 
-const argv = yargs.argv
-var command = argv._[0]
+const notes = require('./notes.js');
 
-console.log('Command:', command);
-console.log('Yargs', argv)
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', argv);
 
-if (command === 'add'){
-    notes.addNote(argv.title, argv.body)
-}else if(command ==='list'){
-    notes.getAll();
-}else if (command === 'read') {
-    notes.getNote(argv.title)
-}else if (command === 'remove') {
-    notes.removeNote(argv.title)
-}else {
-    console.log('Command not recognized')
+if (command === 'add') {
+  var note = notes.addNote(argv.title, argv.body);
+  if(note){
+      console.log('Note created')
+      notes.logNote(note)
+  }else{
+      console.log('Note title taken')
+  }
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  var note = notes.getNote(argv.title);
+  if(note) {
+    console.log('Note found')
+      notes.logNote(note)
+  }else{
+      console.log('Note not found')
+  }
+} else if (command === 'remove') {
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Not was removed' : 'Note not found'
+    console.log(message)
+} else {
+  console.log('Command not recognized');
 }
-
 
 
 
--- ChalkBoard(practice)/Node/NotesNode/notes.js
+++ ChalkBoard(practice)/Node/NotesNode/notes.js
@@ -1,21 +1,72 @@
 console.log('Starting notes.js');
 
+// moduel to  read/ write files
+const fs = require('fs');
+
+var fetchNotes = () => {
+    //if file try to read it for duplicatenotes, if no file do nothing
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    //turns into json
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+    // writes file in string format
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+//build out add functionality
 var addNote = (title, body) => {
-    console.log('Adding note', title, body)
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+// if no match length will be 0 so it'll be pushed onto the array
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
 };
-var getAll = () =>{
-    console.log('Getting all notes')
+
+var getAll = () => {
+  console.log('Getting all notes');
 };
-var getNote = (title) =>{
-    console.log('Getting note',title)
-}
-var removeNote = (title) =>{
-    console.log('Removing note',title)
+
+var getNote = (title) => {
+  var notes = fetchNotes();
+  var filterNotes = notes.filter((note) => note.title === title);
+  return filterNotes[0];
+};
+
+var removeNote = (title) => {
+  //fetch notes
+    var notes = fetchNotes()
+  //filter notes, removing the one with title of argument
+    var filteredNotes = notes.filter((note) => note.title !== title)
+    saveNotes(filteredNotes)
+
+    return notes.length !== filteredNotes.length
+};
+var logNote = (note) => {
+    console.log('---')
+    console.log(`Title: ${note.title}`)
+    console.log(`Body: ${note.body}`)
 }
 
+
+
 module.exports = {
-    addNote,
-    getAll,
-    getNote,
-    removeNote
-}
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+  logNote,
+};
