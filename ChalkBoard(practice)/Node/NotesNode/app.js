bc281506bd9fbaa6753cbd55d0466d6a10630860
Nathan Kinney <nathankeithkinney@gmail.com>
2018-08-11 19:00:56 -0700

    Node Work

--- ChalkBoard(practice)/Node/NotesNode/app.js
+++ ChalkBoard(practice)/Node/NotesNode/app.js
@@ -1,8 +1,12 @@
 console.log('starting app.')
 
 const fs = require('fs')
+const os = require('os')
 
-fs.appendFile('greetings.txt', 'Hello World!', function (err){
+var user = os.userInfo()
+
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err){
     if(err){
         console.log('Unable to write file')
     }
