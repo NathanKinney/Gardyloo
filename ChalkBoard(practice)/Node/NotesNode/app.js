3fa2c8815064d06dd24cea58b4cfabfef282083f
Nathan Kinney <nathankeithkinney@gmail.com>
2018-08-20 15:57:43 -0700

    Node Work

--- ChalkBoard(practice)/Node/NotesNode/app.js
+++ ChalkBoard(practice)/Node/NotesNode/app.js
@@ -5,7 +5,34 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+        describe: 'Title of Note',
+        demand: true,
+        alias: 't'
+      };
+
+const bodyOptions = {
+          describe: 'Body of note',
+          demand: true,
+          alias: 'b'
+      };
+
+const argv = yargs
+    .command('add', 'Add a new Note', {
+        title: titleOptions,
+        body: bodyOptions,
+    })
+
+    .command('list', 'List all notes')
+    .command('read', 'Read a Note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a Note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+
 var command = argv._[0];
 
 if (command === 'add') {
@@ -30,7 +57,7 @@ if (command === 'add') {
   }
 } else if (command === 'remove') {
   var noteRemoved = notes.removeNote(argv.title);
-  var message = noteRemoved ? 'Not was removed' : 'Note not found'
+  var message = noteRemoved ? 'Note was removed' : 'Note not found'
     console.log(message)
 } else {
   console.log('Command not recognized');
