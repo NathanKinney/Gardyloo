cc66c25440ce1b5ca00c542b94b0a42e9d40233d
Nathan Kinney <nathankeithkinney@gmail.com>
2018-08-21 12:06:49 -0700

    Node Work

--- ChalkBoard(practice)/Node/NotesNode/playground/arrow-function.js
+++ ChalkBoard(practice)/Node/NotesNode/playground/arrow-function.js
@@ -1,4 +1,15 @@
+//w
 
-var square = (x) => x*x;
 
-console.log(square(9))
+var user = {
+    name:'Nathan',
+    sayHi: () => {
+        console.log(`Hi. I'm ${this.name}`)
+    },
+    sayHiAlt() {
+        console.log(arguments)
+        console.log(`Hi. I'm ${this.name}`)
+    }
+};
+
+user.sayHiAlt(1,2,3)
