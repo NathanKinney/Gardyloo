449b270e5e0fdc1ffaa249404b1e796c1f01c0b1
Nathan Kinney <nathankeithkinney@gmail.com>
2018-08-21 13:02:26 -0700

    Node Work

---
+++ ChalkBoard(practice)/Node/Weather-App/playground/Async-Basics.js
@@ -0,0 +1,10 @@
+console.log('Starting app')
+
+setTimeout(()=>{
+    console.log('Inside Callback')
+},2000)
+
+setTimeout(()=>{
+    console.log('Second setTimeout')
+},0)
+console.log('Finishing up')
