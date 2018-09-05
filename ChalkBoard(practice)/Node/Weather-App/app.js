821d5716b0329241178278452e292e7131ea1243
Nathan Kinney <nathankeithkinney@gmail.com>
2018-09-05 12:03:56 -0700

    Node Work

--- ChalkBoard(practice)/Node/Weather-App/app.js
+++ ChalkBoard(practice)/Node/Weather-App/app.js
@@ -1,8 +1,24 @@
-const request = require('request')
+const yargs = require('yargs');
 
-request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-    json: true
-}, (error,response,body)=> {
-    console.log(body);
-})
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
+});
