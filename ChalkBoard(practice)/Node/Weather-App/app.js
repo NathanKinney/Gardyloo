24059040acddff8767cb3707cf3414f4e083c94c
Nathan Kinney <nathankeithkinney@gmail.com>
2018-09-05 18:01:28 -0700

    Node Work

--- ChalkBoard(practice)/Node/Weather-App/app.js
+++ ChalkBoard(practice)/Node/Weather-App/app.js
@@ -1,39 +1,32 @@
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
+const yargs = require('yargs');
 
-// api.darksky.net/forecast/a569e0881d0a65b38de1794028db85f5/45.5461375,-122.6766628
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
-const request = require('request');
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
-request({
-  url: 'https://api.darksky.net/forecast/a569e0881d0a65b38de1794028db85f5/45.5461375,-122.6766628',
-  json: true
-}, (error, response, body) => {
-  if (!error && response.statusCode == 200) {
-      console.log(body.currently.temperature)
-  }else {
-      console.log('unable to fetch weather.')
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+      }
+    });
   }
 });
