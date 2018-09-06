5f8d6deeca54907173bc88333bc69d24e1cca978
Nathan Kinney <nathankeithkinney@gmail.com>
2018-09-06 13:17:37 -0700

    Node Work

--- ChalkBoard(practice)/Node/Weather-App/playground/promise.js
+++ ChalkBoard(practice)/Node/Weather-App/playground/promise.js
@@ -1,15 +1,36 @@
-
-var somePromise = new Promise((resolve, reject) => {
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        //resolve('hey It worked')
-        reject('unable to fulfill promise')
-    },2500)
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+    }, 1500);
+  });
+};
 
+asyncAdd(5, '7').then((res) => {
+  console.log('Result: ', res);
+  return asyncAdd(res, 33);
+}).then((res) => {
+  console.log('Should be 45', res);
+}).catch((errorMessage) => {
+  console.log(errorMessage);
 });
 
-somePromise.then((message) => {
-    console.log('Success: ', message)
-}, (errorMessage) => {
-    console.log('Error: ', errorMessage)
-})
+
+// var somePromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         //resolve('hey It worked')
+//         reject('unable to fulfill promise')
+//     },2500)
+//
+// });
+//
+// somePromise.then((message) => {
+//     console.log('Success: ', message)
+// }, (errorMessage) => {
+//     console.log('Error: ', errorMessage)
+// })
 
