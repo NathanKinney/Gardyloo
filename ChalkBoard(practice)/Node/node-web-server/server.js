eb08805f21bd530135fabda4a866d15eb781fc5f
Nathan Kinney <nathankeithkinney@gmail.com>
2018-09-14 16:16:10 -0700

    EXPRESS

--- ChalkBoard(practice)/Node/node-web-server/server.js
+++ ChalkBoard(practice)/Node/node-web-server/server.js
@@ -1,24 +1,51 @@
 const express = require('express');
 const hbs = require('hbs')
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
+// registers middleware "app.use"
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log')
+        }
+    })
+    next();
+})
+
+// never calling next stops propigation order matters
+// app.use((req, res, next) =>{
+//     res.render('maintenance.hbs')
+// })
+
 app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+})
+
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         welcomeMessage: 'Welcome to my Home',
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear(),
+        pageTitle: 'About Page'
     });
 })
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 })
 
